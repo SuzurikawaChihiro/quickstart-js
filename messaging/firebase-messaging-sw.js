14afc564297270a1defc1f7e7e32267aff4cfa74
SuzurikawaChihiro <suzurikawa_chihiro@retail-ai.jp>
2021-02-02 13:00:42 +0900

    Firebase Project 設定

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,16 +1,18 @@
-// Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/8.2.5/firebase-app.js');
-importScripts('/__/firebase/8.2.5/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+// // Import and configure the Firebase SDK
+// // These scripts are made available when the app is served or deployed on Firebase Hosting
+// // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+// importScripts('/__/firebase/8.2.2/firebase-app.js');
+// importScripts('/__/firebase/8.2.2/firebase-messaging.js');
+// importScripts('/__/firebase/init.js');
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
 /**
  * Here is is the code snippet to initialize Firebase Messaging in the Service
  * Worker when your app is not hosted on Firebase Hosting.
+ **/
 
+ // [START initialize_firebase_in_sw]
  // Give the service worker access to Firebase Messaging.
  // Note that you can only use Firebase Messaging here. Other Firebase libraries
  // are not available in the service worker.
@@ -21,34 +23,38 @@ const messaging = firebase.messaging();
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
  firebase.initializeApp({
-   apiKey: 'api-key',
-   authDomain: 'project-id.firebaseapp.com',
-   databaseURL: 'https://project-id.firebaseio.com',
-   projectId: 'project-id',
-   storageBucket: 'project-id.appspot.com',
-   messagingSenderId: 'sender-id',
-   appId: 'app-id',
-   measurementId: 'G-measurement-id',
- });
+  apiKey: "AIzaSyAV3WooK-V3jnz-hb5_3RxUZ8_nr95e0mI",
+  authDomain: "tfs-id-platform.firebaseapp.com",
+  databaseURL: "https://tfs-id-platform.firebaseio.com",
+  projectId: "tfs-id-platform",
+  storageBucket: "tfs-id-platform.appspot.com",
+  messagingSenderId: "315615346351",
+  appId: "1:315615346351:web:85321ea1029ec383775e5a",
+  measurementId: "G-XHS3CKH161"
+});
 
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
  const messaging = firebase.messaging();
- **/
+ // [END initialize_firebase_in_sw]
 
 
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+// // If you would like to customize notifications that are received in the
+// // background (Web app is closed or not in browser focus) then you should
+// // implement this optional method.
+// // [START on_background_message]
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     // body: 'Background Message body.',
+//     body: payload,
+//     icon: '/firebase-logo.png'
+//   };
+
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
+// // [END on_background_message]
